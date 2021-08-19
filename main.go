74f17c3770c15eadf1456751f19328bb4b42a2c9
qsmsoft <qsmsoft@gmail.com>
2021-08-19 23:02:28 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/qsmsoft/parsingJSONandXML/feeds"
+	"github.com/qsmsoft/parsingJSONandXML/itunes"
+	"log"
+)
+
+func main() {
+	ias := itunes.NewItunesApiServices()
+
+	res, err := ias.Search("Full Stack Radio")
+	if err != nil {
+		log.Fatalf("error while searching: %v", err)
+	}
+	for _, item := range res.Results {
+		log.Println("---------------------------------")
+		log.Printf("Artist: %s", item.ArtistName)
+		log.Printf("Podcast Name: %s", item.TrackName)
+		log.Printf("Feed url: %s", item.FeedURL)
+
+		feed, err := feeds.GetFeed(item.FeedURL)
+		if err != nil {
+			log.Fatalf("error while searching: %v", err)
+		}
+
+		for _, pod := range feed.Channel.Item {
+			log.Println("-----------------------------")
+			log.Printf("Title: %s", pod.Title)
+			log.Printf("Duration: %s", pod.Duration)
+			log.Printf("Description: %s", pod.Description)
+			log.Printf("URL: %s", pod.Enclosure.URL)
+			log.Println("-----------------------------")
+		}
+
+		log.Println("---------------------------------")
+	}
+}
